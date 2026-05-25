1cccc5060cd4d8bfc3b19f1541cdcfac88baa243
HanaF02 <139299804+HanaF02@users.noreply.github.com>
2026-05-25 15:52:49 +0300

    remove commented code
    Removed commented-out CORS configuration and cleaned up the code.

--- src/app.js
+++ src/app.js
@@ -7,22 +7,12 @@ import logRoutes from "./routes/logRoutes.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 import dotenv from "dotenv";
 dotenv.config({ quiet: true });
-
-const app = express(); // Create an Express application
-
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true })); // Enable CORS for the specified client URL and allow credentials (cookies) to be sent in cross-origin requests
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     credentials: true,
-//   }),
-// );
-app.use(express.json()); // Middleware to parse incoming JSON requests and make the data available in req.body
-app.use(cookieParser()); // Middleware to parse cookies from incoming requests and make them available in req.cookies
-
-app.use("/api/users", authRoutes); // Use the authRoutes for any requests to /api/users, which will handle user authentication and related operations
-app.use("/api/applications", logRoutes); // Use the logRoutes for any requests to /api/logs, which will handle log-related operations
-app.use("/api/applications", applicationRoutes); // Use the applicationRoutes for any requests to /api/applications, which will handle operations related to applications
-
-app.use(errorHandler); // Use the custom error handling middleware to catch and handle errors that occur in the application
-export default app; // Export the Express application for use in server.js, where it will be imported and used to start the server
+const app = express(); 
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true })); 
+app.use(express.json()); 
+app.use(cookieParser()); 
+app.use("/api/users", authRoutes); 
+app.use("/api/applications", logRoutes); 
+app.use("/api/applications", applicationRoutes);
+app.use(errorHandler); 
+export default app; 
