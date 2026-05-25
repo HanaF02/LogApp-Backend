5f4cce8a707f4efc2c010844ce136f0018f3dae7
HanaF02 <139299804+HanaF02@users.noreply.github.com>
2026-05-25 15:45:56 +0300

    Refactor authController for cleaner code

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,26 +1,25 @@
 import Developer from "../models/Developer.js";
 import jwt from "jsonwebtoken";
 const generateToken = (
-  id, //fn to gen token
+  id, 
 ) => jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1d" });
 const cookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
   sameSite: "strict",
-  maxAge: 24 * 60 * 60 * 1000, //1 day in ms
+  maxAge: 24 * 60 * 60 * 1000, 
 };
 export const register = async (req, res, next) => {
   try {
-    const { username, email, password } = req.body; //get new user info sent from post req
+    const { username, email, password } = req.body;
     const exists = await Developer.findOne({ $or: [{ email }, { username }] });
     if (exists) {
-      // tell the user exactly which field is taken
       const field = exists.email === email ? "Email" : "Username";
       return res.status(400).json({ message: `${field} already in use` });
     }
-    const developer = await Developer.create({ username, email, password }); //else create this new user
-    const token = generateToken(developer._id); //gen a token for this user to login with later and verify them
-    res.cookie("token", token, cookieOptions); //This sets an HTTP Header (Set-Cookie) in the server response. The Result: The browser intercepts this header, creates a secure file in its private storage named "token", and pastes your encrypted JWT string inside it
+    const developer = await Developer.create({ username, email, password });
+    const token = generateToken(developer._id); 
+    res.cookie("token", token, cookieOptions); 
     res.status(201).json({
       _id: developer._id,
       username: developer.username,
@@ -39,7 +38,7 @@ export const login = async (req, res, next) => {
       //not found or wrong cred
       return res.status(401).json({ message: "Invalid credentials" });
     }
-    const token = generateToken(developer._id); //??
+    const token = generateToken(developer._id); 
     res.cookie("token", token, cookieOptions);
     res.json({
       _id: developer._id,
